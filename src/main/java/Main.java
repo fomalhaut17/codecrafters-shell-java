0000b9455f6ba8327949078a46bfccf71d955ab6
leewonseok <fomalhaut17@gmail.com>
2025-11-13 09:53:52 +0900

    type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+  private static List<String> builtinCommands = List.of("exit", "echo", "type");
+
   public static void main(String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
 
@@ -17,6 +20,13 @@ public class Main {
           parameters = parameters.trim();
         }
         System.out.println(parameters);
+      } else if ("type".equals(command)) {
+        String parameter = scanner.next();
+        if (builtinCommands.contains(parameter)) {
+          System.out.println(parameter + " is a shell builtin");
+        } else {
+          System.out.println(parameter + ": not found");
+        }
       } else {
         System.out.println(command + ": command not found");
       }
