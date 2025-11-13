5d734f5f69a8ded65ec8f0d256ef8f40ce65e134
leewonseok <fomalhaut17@gmail.com>
2025-11-13 11:56:58 +0900

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,29 @@
 import java.io.File;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Predicate;
 
 public class Main {
   private static List<String> builtinCommands = List.of("exit", "echo", "type");
 
   private static String[] parseCommand(String commandWithArguments) {
     return commandWithArguments.split("\s", 2);
+  }
+
+  private static boolean findInPath(String name, String[] paths, Predicate<File> filePredicate) {
+    for (String path : paths) {
+      File directory = new File(path);
+      File[] listFiles = directory.listFiles();
+      if (listFiles != null) {
+        for (File file : listFiles) {
+          if (filePredicate.test(file)) {
+            System.out.println(name + " is " + file.getAbsolutePath());
+            return true;
+          }
+        }
+      }
+    }
+    return false;
   }
 
   public static void main(String[] args) throws Exception {
@@ -29,23 +46,7 @@ public class Main {
           System.out.println(arguments + " is a shell builtin");
         } else {
           String[] paths = System.getenv("PATH").split(File.pathSeparator);
-          boolean findFlag = false;
-
-          outer:
-          for (String path : paths) {
-            File directory = new File(path);
-            File[] listFiles = directory.listFiles();
-            if (listFiles != null) {
-              for (File file : listFiles) {
-                if (file.getName().equals(arguments) && file.canExecute()) {
-                  System.out.println(arguments + " is " + file.getAbsolutePath());
-                  findFlag = true;
-                  break outer;
-                }
-              }
-            }
-          }
-
+          boolean findFlag = findInPath(arguments, paths, file -> file.getName().equals(arguments) && file.canExecute());
           if (!findFlag) {
             System.out.println(arguments + ": not found");
           }
