aa0741a39a7c0c7c9888a166fbc1c7505abf395a
leewonseok <fomalhaut17@gmail.com>
2025-11-13 13:18:14 +0900

    execute

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,59 +1,97 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class Main {
-  private static List<String> builtinCommands = List.of("exit", "echo", "type");
+    private static List<String> builtinCommands = List.of("exit", "echo", "type");
 
-  private static String[] parseCommand(String commandWithArguments) {
-    return commandWithArguments.split("\s", 2);
-  }
-
-  private static boolean findInPath(String name, String[] paths, Predicate<File> filePredicate) {
-    for (String path : paths) {
-      File directory = new File(path);
-      File[] listFiles = directory.listFiles();
-      if (listFiles != null) {
-        for (File file : listFiles) {
-          if (filePredicate.test(file)) {
-            System.out.println(name + " is " + file.getAbsolutePath());
-            return true;
-          }
-        }
-      }
+    private static String[] parseCommand(String commandWithArguments) {
+        return commandWithArguments.split("\s", 2);
     }
-    return false;
-  }
 
-  public static void main(String[] args) throws Exception {
-    Scanner scanner = new Scanner(System.in);
+    private static boolean findFile(String[] paths, String name, Predicate<File> filePredicate, Consumer<File> fileConsumer) {
+        for (String path : paths) {
+            File directory = new File(path);
+            File[] listFiles = directory.listFiles();
+            if (listFiles != null) {
+                for (File file : listFiles) {
+                    if (file.getName().equals(name) && filePredicate.test(file)) {
+                        if (fileConsumer != null) {
+                            fileConsumer.accept(file);
+                        }
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 
-    while (true) {
-      System.out.print("$ ");
+    private static void executeProcess(String[] paths, String command, String arguments) {
+        boolean isFound = findFile(paths, command, f -> f.canExecute(), null);
+        if (isFound) {
+            List<String> commands = new ArrayList<>();
+            commands.add(command);
+            if (arguments != null) {
+                commands.add(arguments);
+            }
 
-      String commandWithArguments = scanner.nextLine();
-      String[] parts = parseCommand(commandWithArguments);
-      String command = parts[0];
-      String arguments = parts.length > 1 ? parts[1] : null;
-
-      if ("exit".equals(command)) {
-        System.exit(Integer.parseInt(arguments));
-      } else if ("echo".equals(command)) {
-        System.out.println(arguments);
-      } else if ("type".equals(command)) {
-        if (builtinCommands.contains(arguments)) {
-          System.out.println(arguments + " is a shell builtin");
+            ProcessBuilder pb = new ProcessBuilder(commands);
+            pb.redirectErrorStream(true);
+            try {
+                Process process = pb.start();
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        System.out.println(line);
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         } else {
-          String[] paths = System.getenv("PATH").split(File.pathSeparator);
-          boolean isFound = findInPath(arguments, paths, file -> file.getName().equals(arguments) && file.canExecute());
-          if (!isFound) {
-            System.out.println(arguments + ": not found");
-          }
+            System.out.println(command + ": not found");
         }
-      } else {
-        System.out.println(commandWithArguments + ": command not found");
-      }
     }
-  }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("$ ");
+
+            String commandWithArguments = scanner.nextLine();
+            String[] parts = parseCommand(commandWithArguments);
+            String command = parts[0];
+            String arguments = parts.length > 1 ? parts[1] : null;
+
+            String[] paths = System.getenv("PATH").split(File.pathSeparator);
+
+            if (builtinCommands.contains(command)) {
+                if ("exit".equals(command)) {
+                    System.exit(Integer.parseInt(arguments));
+                } else if ("echo".equals(command)) {
+                    System.out.println(arguments);
+                } else if ("type".equals(command)) {
+                    if (builtinCommands.contains(arguments)) {
+                        System.out.println(arguments + " is a shell builtin");
+                    } else {
+                        boolean isFound = findFile(paths, arguments, f -> f.canExecute(), f -> System.out.println(arguments + " is " + f.getAbsolutePath()));
+                        if (!isFound) {
+                            System.out.println(arguments + ": not found");
+                        }
+                    }
+                }
+            } else {
+                executeProcess(paths, command, arguments);
+            }
+        }
+    }
+
 }
